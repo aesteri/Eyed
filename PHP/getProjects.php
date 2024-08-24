5bb451f5989204209498c168e14fc69ad585ba75
Christine <132790780+aesteri@users.noreply.github.com>
2024-08-24 03:06:11 -0400

    fixed cross browser PHP issues

--- PHP/getProjects.php
+++ PHP/getProjects.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $servername = "localhost";
 $username = "christs0_christine";
 $password = "Gu(OA}1HXWL(";
